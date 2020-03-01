e2a20e6204f71f86ea7ced8aa1fcd03226e806d4
Peter Johnson <pj16aac@herts.ac.uk>
2020-03-01 17:45:53 +0100

    Created DB models for listings and staff

--- staff/models.py
+++ staff/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from datetime import datetime
+
+class Staff(models.Model):
+    name = models.CharField(max_length=200)
+    job_title = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    start_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name
