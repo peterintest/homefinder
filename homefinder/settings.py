fb1842cb3132a29c5dc7d69540ea9673f057c114
Peter Johnson <pj16aac@herts.ac.uk>
2020-03-09 14:00:57 +0000

    Created contacts django app and model

--- homefinder/settings.py
+++ homefinder/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'staff.apps.StaffConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
