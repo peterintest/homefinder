414f5d17f507176746ddc457131f11d73f38fc28
Peter Johnson <pj16aac@herts.ac.uk>
2020-03-09 10:54:40 +0000

    Added logic to login a user in Login View

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 def register(request):
@@ -38,6 +38,19 @@ def register(request):
         return render(request, 'accounts/register.html')
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('index')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 def logout(request):
