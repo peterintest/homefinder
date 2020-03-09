c738d195f402db19a098454f75627ce651625e8f
Peter Johnson <pj16aac@herts.ac.uk>
2020-03-09 09:45:04 +0000

    Added logic to register a user in Register View

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # check passwords
+        if password == password2:
+            # check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, "Username is already taken")
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, "E-mail already registered")
+                    return redirect('register')
+                else:
+                    # register user
+                    user = User.objects.create_user(username=username,
+                                                    password=password,
+                                                    email=email,
+                                                    first_name=first_name,
+                                                    last_name=last_name)
+                    user.save()
+                    messages.success(request, 'Account registered successfully')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 def login(request):
     return render(request, 'accounts/login.html')
