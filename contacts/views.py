d19576bf0d7ebd56dd88761f88027ed0bd71c32a
Peter Johnson <pj16aac@herts.ac.uk>
2020-03-09 18:51:20 +0000

    Added logic for saving an enquiry

--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Contact
 
 def contact(request):
-    return
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+
+        contact = Contact(listing=listing,
+                          listing_id=listing_id,
+                          name=name,
+                          email=email,
+                          phone=phone,
+                          message=message)
+
+        contact.save()
+
+        messages.success(request, 'Your message has been sent')
+        return redirect(f'/listings/{listing_id}')
