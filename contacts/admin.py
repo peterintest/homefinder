90059eb1cf06b9003e5dd93f64e45beb3790da0b
Peter Johnson <pj16aac@herts.ac.uk>
2020-04-23 11:26:06 +0100

    Prevent contact enquiry details from being modified in admin view

--- contacts/admin.py
+++ contacts/admin.py
@@ -7,5 +7,15 @@ class ContactAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name')
     search_fields = ('name', 'email', 'listing')
     list_per_page = 25
+    readonly_fields = [
+        'name',
+        'listing',
+        'listing_id',
+        'name',
+        'email',
+        'phone',
+        'message',
+        'contact_date'
+    ]
 
 admin.site.register(Contact, ContactAdmin)
